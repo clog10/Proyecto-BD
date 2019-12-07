14993e47221c62dd1d1402787677643b5a0e053e
Clog_10 <Clog_10@DESKTOP-7MBN3I4>
2019-12-06 22:30:16 -0600

    Primera Versión Librerias a Usar (Driver y Tema)

---
+++ src/modelo/conectate.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplodb;
+
+import java.sql.*; 
+
+public class conectate { 
+   static String bd = "inegi"; 
+   static String login = "root"; 
+   static String password = "12345"; 
+   static String url = "jdbc:mysql://localhost/"+bd; 
+ 
+   Connection conn = null; 
+    
+   public conectate() { 
+      try{          
+         Class.forName("com.mysql.jdbc.Driver");          
+         conn = DriverManager.getConnection(url,login,password); 
+         if (conn!=null){ 
+            System.out.println("Conexión a base de datos "+bd+". listo"); 
+         } 
+      }catch(SQLException e){ 
+         System.out.println(e); 
+      }catch(ClassNotFoundException e){ 
+         System.out.println(e); 
+      } 
+   } 
+    
+   public Connection getConnection(){ 
+      return conn; 
+   } 
+ 
+   public void desconectar(){ 
+      conn = null; 
+   } 
+}
